811806a19de9dcb122ee84a74e8e4b5119a95283
Julie357 <julie.barbouxbitauld@gmail.com>
2023-11-22 16:21:32 +0100

    Classe - pas fini

--- src/Compoments/Classe.jsx
+++ src/Compoments/Classe.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import Box, { BoxProps } from '@mui/material/Box';
+import Box from '@mui/material/Box';
+import { ViewList as ViewListIcon, ViewModule as ViewModuleIcon, ViewQuilt as ViewQuiltIcon, ArrowBack as ArrowBackIcon,ArrowForward as ArrowForwardIcon} from '@mui/icons-material';
+import {ToggleButton, ToggleButtonGroup, Switch, Pagination, PaginationItem, FormControlLabel} from '@mui/material';
+import { BoxProps } from '@mui/material/Box';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Item(props: BoxProps) {
   const { sx, ...other } = props;
@@ -28,25 +38,104 @@ function Item(props: BoxProps) {
 
 const PageEleve = () => {
   return (
-    // <Box sx={{width: '120px', height: '120px', textAlign: 'center', boxShadow: 4, borderRadius: '29px' }}>
-    // <Typography sx={{fontSize: '3rem', fontWeight: '500', p: 1, m:1}}>
-    //   6°1
-    // </Typography>
-    // </Box>
-    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', border: '1px solid red' }}>
-      <Item>1</Item>
-      <Item>2</Item>
-      <Item>3</Item>
-      <Item>1</Item>
-      <Item>2</Item>
-      <Item>3</Item>
-      <Item>1</Item>
-      <Item>2</Item>
-      <Item>3</Item>
-      <Item>1</Item>
-      <Item>2</Item>
-      <Item>3</Item>
+    <Box>
+      <Box sx={{p:2, display: 'flex', width: '100%', alignContent: 'center'}}>
+        <Box sx={{width: '10%', border: '1px solid red', textAlign: 'center', alignContent: 'center'}}>Logo</Box>
+
+        <Box sx={{width: '15%', mx: '5%'}}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Age"
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Box sx={{width: '20%', border: '1px solid red'}}>Filtre</Box>
+
+        <Box sx={{width: '20%', mx: '5%', display:'flex'}}>
+  
+
+         
+          <Box sx={{border: '1px solid red'}}>
+            <FormControl fullWidth>
+              <InputBase
+                placeholder="Rechercher..."
+                startAdornment={<SearchIcon />}
+                sx={{ ml: 1, flex: 1 }}
+              />
+            </FormControl>
+          </Box>
+        </Box>
+
+
+
+      </Box>
+
+
+      <Box sx={{ m: 2, p: 2, background: '#D9D9D9'}}>
+        <ToggleButtonGroup
+            exclusive
+          >
+            <ToggleButton value="list" aria-label="list">
+              <ViewListIcon />
+            </ToggleButton>
+            <ToggleButton value="module" aria-label="module">
+              <ViewModuleIcon />
+            </ToggleButton>
+            <ToggleButton value="quilt" aria-label="quilt">
+              <ViewQuiltIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+      
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', my: 2 }}>
+          <Item>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+          <Item>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+          <Item>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+          <Item>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+        </Box>
+
+        <Box sx={{textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
+          <Pagination
+            count={10}
+            renderItem={(item) => (
+              <PaginationItem
+                slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+                {...item}
+              />
+            )}
+          />
+          
+        </Box>
+        <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+          <FormControlLabel
+            control={<Switch color="primary" />}
+            label="Afficher toutes les classes"
+            labelPlacement="start"
+          />
+        </Box>
+        
     </Box>
+
+    <Box>
+    
+    </Box>
+    </Box>
+    
   );
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,14 +5,7 @@ import Classe from './Compoments/Classe';
 
 const Home = () => {
   return (
-    <div
-      sx={{padding: 20, backgroundColor: '#f0f0f0' }}
-    >
-      <h1>Bienvenue dans mon projet React</h1>
-      <p>Ceci est un composant JSX de base.</p>
-      <Eleve />
       <Classe />
-    </div>
   );
 };
 
