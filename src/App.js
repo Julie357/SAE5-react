9b09d986b2ecaaefe1b6ab3ffaf3b3ef76187048
KhTaeron <120678815+KhTaeron@users.noreply.github.com>
2023-11-22 16:23:30 +0100

    configuration des routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-
-function App() {
+import StudentCard from './pages/StudentCard';
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/studentCard" element={<StudentCard/>} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
