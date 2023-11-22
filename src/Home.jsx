a92ff7d55ea4d2b384234e2de05ad2fe5ee9081d
Julie357 <julie.barbouxbitauld@gmail.com>
2023-11-22 14:57:29 +0100

    Home

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+// src/MonComposant.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenue dans mon projet React</h1>
+      <p>Ceci est un composant JSX de base.</p>
+    </div>
+  );
+};
+
+export default Home;
