7380ab6647ec9129af0861ae0fcc1053e77be355
Luleil <104360837+Luleil@users.noreply.github.com>
2023-11-23 10:10:37 +0100

    route 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 // src/App.js
 import React from 'react';
 import Home from './pages/Home';
+import Page404 from './pages/Page404';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+     <div className="App">
       <Routes>
         <Route>
-          <div className="App">
-            <Home />
-          </div>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="*" element={<Page404/>} />
         </Route>
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page404 = () => {
+  return (
+   <div>
+       <h1>Il n'y a rien ici</h1>
+   </div>
+  );
+};
+
+export default Page404;
