6e54851a99cc36372c32ca1e377236f5954fbb5c
Luleil <104360837+Luleil@users.noreply.github.com>
2023-11-23 12:15:01 +0100

    page 404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,25 +1,33 @@
 import { Button } from '@mui/material';
 import React from 'react';
 import svg from "../assets/404-page-not-found.svg";
+import svg2 from "../assets/apple-logo.svg";
 import { NavLink } from 'react-router-dom';
 import "../css/page404.css";
 
 const Page404 = () => {
     return (
-        <div className="cont-404">
-            <img src={svg} alt="Page 404" />
-            <NavLink exact to="/">
+        <div>
+            <div className="logodelapage">
+            <img className="logo" src={svg2} alt="logo A4ll" />
+            </div>
+            
+            <div className="cont-404">
+                <img src={svg} alt="Page 404" />
+                <h1>On dirait bien que vous êtes perdu !</h1>
+                <NavLink exact to="/">
 
-                <Button sx={{
-                    p: "12px 15px", backgroundColor: "rgb(255, 20, 235)", boder: "none",
-                    boderRadius: "12px", cursor: "pointer", color: "#FFF", transition: "0.3s", fontSize: "medium", fontWeight:"bold", 
+                    <Button sx={{
+                        p: "12px 15px", backgroundColor: "rgb(255, 20, 235)", boder: "none",
+                        boderRadius: "12px", cursor: "pointer", color: "#FFF", transition: "0.3s", fontSize: "medium", fontWeight: "bold",
 
-                    '&:hover': {
-                        backgroundColor: 'darkblue',
-                    },
-                }}>revenir à l'accueil</Button>
+                        '&:hover': {
+                            backgroundColor: 'darkblue',
+                        },
+                    }}>revenir à l'accueil</Button>
 
-            </NavLink>
+                </NavLink>
+            </div>
         </div>
     );
 };
