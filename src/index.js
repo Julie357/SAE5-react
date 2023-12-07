c23c46f51837dbc31ad92850281ce197d4e3ba0b
Julie357 <julie.barbouxbitauld@gmail.com>
2023-12-07 14:00:11 +0100

    t

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-
+import Page404 from './pages/Page404';
 import Home from './Home';
 import PageClasses from './Pages/PageClasse';
 import ClasseInformations from './Pages/ClasseInformations';
@@ -19,6 +19,7 @@ root.render(
         <Route path="/classe-informations" element={<ClasseInformations />} />
         <Route path="/texte" element={<PageTexte />} />
         <Route path="/classe" element={<EnsembleClasse />}/>
+        <Route exact path="*" element={<Page404/>} />
 
       </Routes>
     </Router>
