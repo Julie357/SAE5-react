0ab7dfde32f4eca522bbbf5e966ae0aba6bb3c9c
Luleil <104360837+Luleil@users.noreply.github.com>
2023-12-08 14:31:42 +0100

    visu sans données

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './pages/Home';
 import Page404 from './pages/Page404';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dash from './pages/Dash';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route>
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/dash" element={<Dash/>} />
         <Route exact path="*" element={<Page404/>} />
         
         </Route>
---
+++ src/pages/Dash.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Dashboard from '../composants/Dashboard';
+
+
+const Dash = () => {
+  return (
+    <div>
+      <h1>Le dashboard en question</h1>
+      <Dashboard/>
+      </div>
+  );
+};
+
+export default Dash;
