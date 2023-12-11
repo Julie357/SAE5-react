0ad18d85fd7444389de4dfee7b379108a1cc8baa
Julie357 <julie.barbouxbitauld@gmail.com>
2023-12-11 16:28:51 +0100

    Update FilterClasse.jsx

--- src/pages/components/FilterClasse.jsx
+++ src/pages/components/FilterClasse.jsx
@@ -1,44 +1,63 @@
 // FilterClasse.jsx
 import React, { useState } from "react";
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import { FormControl, FormControlLabel, Checkbox, Box, InputLabel, Select, MenuItem, TextField, Button } from "@mui/material";
 
 const FilterClasse = ({ onFilterChange }) => {
   const [filterValue, setFilterValue] = useState("toutes");
 
+  const [filters, setFilters] = useState({
+    level: "",
+    date: "",
+    correction: false,
+  });
+
   const handleFilterChange = (event) => {
-    const value = event.target.value;
-    setFilterValue(value);
-    onFilterChange(value);
+    const { name, value, type, checked } = event.target;
+
+    const filterValue = type === "checkbox" ? checked : value;
+
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: filterValue,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onFilterChange(filters);
   };
 
   return (
-    <FormControl>
-      <FormLabel id="filter-menu-buttons-group-label" sx={{ paddingLeft: "0.4vw" }}>
-        Filtrer par :
-      </FormLabel>
-      <RadioGroup
-        aria-labelledby="filter-menu-buttons-group-label"
-        value={filterValue} // Assurez-vous de spécifier la valeur sélectionnée
-        onChange={handleFilterChange} // Gérez le changement de valeur
-        name="radio-buttons-group"
-        sx={{ paddingLeft: "0.4vw" }}
-      >
-        {["toutes", "avecExercice", "sansExercice"].map((value) => (
-          <FormControlLabel
-            key={value}
-            value={value}
-            control={<Radio />}
-            label={
-              value === "toutes"
-                ? "Toutes les classes"
-                : value === "avecExercice"
-                ? "Avec exercice"
-                : "Sans exercice"
-            }
-          />
-        ))}
-      </RadioGroup>
-    </FormControl>
+    <Box p={2}>
+      <form onSubmit={handleSubmit}>
+        <FormControl fullWidth sx={{ mb: 2 }}>
+          <InputLabel id="level-label">Niveau</InputLabel>
+          <Select
+            labelId="level-label"
+            id="level"
+            name="level"
+            value={filters.level}
+            label="Niveau"
+            onChange={handleFilterChange}
+          >
+            <MenuItem value="">Sélectionnez un niveau</MenuItem>
+            <MenuItem value="A1">6eme</MenuItem>
+            <MenuItem value="A2">5eme</MenuItem>
+            <MenuItem value="B1">4eme</MenuItem>
+            <MenuItem value="B2">3eme</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControlLabel
+          control={<Checkbox checked={filters.correction} onChange={handleFilterChange} name="correction" />}
+          label="Corrigé"
+        />
+
+        <Button type="submit" variant="contained" color="primary">
+          Filtrer
+        </Button>
+      </form>
+    </Box>
   );
 };
 
