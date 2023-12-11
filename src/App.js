dcd9b0ff6054c95caa007b008f548ccb1c1b93a6
Julie357 <106165281+Julie357@users.noreply.github.com>
2023-12-11 10:36:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import PageClasses from './pages/PageClasses';
 import PageTexte from './pages/PageTexte'
 import EnsembleClasse from './pages/EnsembleClasse';
 import Page404 from './pages/Page404';
-import ClasseInformations from './pages/ClasseInformations';
 import { ThemeProvider } from "@mui/material";
 import theme from './theme';
 
@@ -32,7 +31,6 @@ const App = () => {
           <Route path="/studentCard" element={<StudentCard/>} />
           <Route path="/exercicesList" element={<ExercicesList/>} />
           <Route path="/pageClasse" element={<PageClasses/>} />
-          <Route path="/classe-informations" element={<ClasseInformations/>} />
           <Route path="/texte" element={<PageTexte />} />
           <Route path="/classe" element={<EnsembleClasse />}/>
           <Route exact path="*" element={<Page404/>} />
