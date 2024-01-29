ff839e42def8ce92a0eb036514e61cb25873cc8f
KhTaeron <120678815+KhTaeron@users.noreply.github.com>
2024-01-29 16:49:34 +0100

    fix: un mois en fr

--- src/pages/components/Dashboard.jsx
+++ src/pages/components/Dashboard.jsx
@@ -1,124 +1,144 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../../Styles/dashboard.css";
-import '@fontsource/itim';
-import moment from 'moment';
-import { fr } from 'moment/locale/fr';
-
-
+import "@fontsource/itim";
+import moment from "moment";
+import { fr } from "moment/locale/fr";
 
 const DayNames = {
-    0: 'Lun',
-    1: 'Mar',
-    2: 'Mer',
-    3: 'Jeu',
-    4: 'Ven',
-    5: 'Sam',
-    6: 'Dim'
-}
+  0: "Lun",
+  1: "Mar",
+  2: "Mer",
+  3: "Jeu",
+  4: "Ven",
+  5: "Sam",
+  6: "Dim",
+};
 
-const Cell = ({ date, alpha, exercise}) => {
-    const [hoveredInfo, setHoveredInfo] = useState(null);
-    let style = {
-        backgroundColor: `rgba(255, 160, 143, ${alpha})` ,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: '12px',
-        color: '#2b3643',
-        fontFamily: 'Itim',
-        paddingTop: '2px',
-    };
-    return (
-        <div className="cell" style={style} onMouseEnter={() => setHoveredInfo({ date: date.format('DD/MM/YYYY'), value: alpha })}
-        onMouseLeave={() => setHoveredInfo(null)}>
-
-{hoveredInfo && (
-                <div className="hover-info" style={{width:"100px"}}>
-                    <p>{hoveredInfo.date}</p>
-                    <p>{hoveredInfo.value}</p>
-                </div>
-            )}
+const Cell = ({ date, alpha, exercise }) => {
+  const [hoveredInfo, setHoveredInfo] = useState(null);
+  let style = {
+    backgroundColor: `rgba(255, 160, 143, ${alpha})`,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "12px",
+    color: "#2b3643",
+    fontFamily: "Itim",
+    paddingTop: "2px",
+  };
+  return (
+    <div
+      className="cell"
+      style={style}
+      onMouseEnter={() =>
+        setHoveredInfo({ date: date.format("DD/MM/YYYY"), value: alpha })
+      }
+      onMouseLeave={() => setHoveredInfo(null)}
+    >
+      {hoveredInfo && (
+        <div className="hover-info" style={{ width: "100px" }}>
+          <p>{hoveredInfo.date}</p>
+          <p>{hoveredInfo.value}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 const Week = ({ index }) => {
-    return (
-        <div className="week">{DayNames[index]}</div>
-    );
+  return <div className="week">{DayNames[index]}</div>;
 };
 
 const Mois = ({ startDate, index }) => {
-    let date = moment(startDate).add(index * 7, 'day');
-    let MoisName = date.format('MMM');
-    return (
-        <div className={MoisName}>
-            <div className="mois">
+  let date = moment(startDate).add(index * 7, "day");
+  let MoisName = date.format("MMM");
 
-                {MoisName}
-            </div>
-        </div>
-    );
+  const retirerPointDesMois = (mois) => {
+    if (mois.endsWith(".")) {
+      return mois.slice(0, -1);
+    }
+    return mois;
+  };
+
+  MoisName = retirerPointDesMois(MoisName);
+
+  console.log(MoisName);
+  return (
+    <div className={MoisName}>
+      <div className="mois">{MoisName}</div>
+    </div>
+  );
 };
 
 const Timeline = ({ range, data, studentExercises }) => {
-    let days = Math.abs(range[0].diff(range[1], 'days'));
-    let cells = Array.from(new Array(days));
-    let weeks = Array.from(new Array(7));
-    let mois = Array.from(new Array(Math.floor(days / 7)));
-    let startDate = range[0];
-    
-    let min = Math.min(0, ...data.map(d => d.value));
-    let max = Math.max(...data.map(d => d.value));
+  let days = Math.abs(range[0].diff(range[1], "days"));
+  let cells = Array.from(new Array(days));
+  let weeks = Array.from(new Array(7));
+  let mois = Array.from(new Array(Math.floor(days / 7)));
+  let startDate = range[0];
 
-    let colorPerPoint = 1 / (max - min);
-    const DayFormat = 'DDMMYYYY';
+  let min = Math.min(0, ...data.map((d) => d.value));
+  let max = Math.max(...data.map((d) => d.value));
 
-    return (
-        <div className="timeline">
-            <div className="timeline-mois">
-                {mois.map((_, index) => <Mois key={index} index={index} startDate={startDate} />)}
-            </div>
-            <div className="timeline-body">
-                <div className="timeline-weeks">
-                    {weeks.map((_, index) => <Week key={index} index={index} startDate={startDate} />)}
-                </div>
+  let colorPerPoint = 1 / (max - min);
+  const DayFormat = "DDMMYYYY";
 
-                <div className="timeline-cells">
-                    {cells.map((_, index) => {
-                        
-                        let date = moment(startDate).add(index, 'day');
-                        let dataPoint = data.find(d =>  moment(date).format(DayFormat) === moment(d.date).format(DayFormat) )
-                        let alpha = colorPerPoint * dataPoint.value;
-                        return (
-
-
-                            <Cell key={index} index={index} date={date} alpha={alpha} exercise={studentExercises} />
-                        );
-                    })}
-                </div>
-            </div>
+  return (
+    <div className="timeline">
+      <div className="timeline-mois">
+        {mois.map((_, index) => (
+          <Mois key={index} index={index} startDate={startDate} />
+        ))}
+      </div>
+      <div className="timeline-body">
+        <div className="timeline-weeks">
+          {weeks.map((_, index) => (
+            <Week key={index} index={index} startDate={startDate} />
+          ))}
         </div>
-    );
+
+        <div className="timeline-cells">
+          {cells.map((_, index) => {
+            let date = moment(startDate).add(index, "day");
+            let dataPoint = data.find(
+              (d) =>
+                moment(date).format(DayFormat) ===
+                moment(d.date).format(DayFormat)
+            );
+            let alpha = colorPerPoint * dataPoint.value;
+            return (
+              <Cell
+                key={index}
+                index={index}
+                date={date}
+                alpha={alpha}
+                exercise={studentExercises}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const Dashboard = ({studentExercises}) => {
-      
-    let startDate = moment().add(-365, 'days');
-    let dateRange = [startDate, moment()];
-    let data = Array.from(new Array(365)).map((_, index) => {
+const Dashboard = ({ studentExercises }) => {
+  let startDate = moment().add(-365, "days");
+  let dateRange = [startDate, moment()];
+  let data = Array.from(new Array(365)).map((_, index) => {
+    return {
+      date: moment(startDate).add(index, "day"),
+      value: Math.floor(Math.random() * 100),
+    };
+  });
 
-        return {
-            date: moment(startDate).add(index, 'day'),
-            value: Math.floor(Math.random() * 100)
-        };
-    });
-
-    console.log(studentExercises);
-    return (
-        <Timeline range={dateRange} data={data} studentExercises={studentExercises} />
-     
-    );
+  console.log(studentExercises);
+  return (
+    <Timeline
+      range={dateRange}
+      data={data}
+      studentExercises={studentExercises}
+    />
+  );
 };
-
 
 export default Dashboard;
