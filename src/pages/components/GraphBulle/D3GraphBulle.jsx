d6510b7f65ae9e52ea72e80c851c0a293d0747ca
Julie357 <julie.barbouxbitauld@gmail.com>
2024-02-05 15:34:37 +0100

    texte

--- src/pages/components/GraphBulle/D3GraphBulle.jsx
+++ src/pages/components/GraphBulle/D3GraphBulle.jsx
@@ -20,7 +20,7 @@ const ForceDirectedGraph = () => {
       .range(["#A1CDF1", "#FFB5A7", "#FFE6E2", "#3D6787", "#A1CDF1", "#D8ECFC"]);
 
     const data1 = [{ "name": "nom", "group": 1, "size": 20 }, { "name": "conjugaison", "group": 2, "size": 60 }, { "name": "be + ing", "group": 3, "size": 40 }, { "name": "pronom", "group": 4, "size": 50 }, { "name": "adjectif", "group": 5, "size": 30 }, { "name": "verbe", "group": 6, "size": 40 }];
-    
+
     const data = [];
     recurrentErrors.forEach((recurrentError, index) => {
       const category = Object.keys(recurrentError)[0];
@@ -68,8 +68,42 @@ const ForceDirectedGraph = () => {
       .text(d => d.name)
       .attr("text-anchor", "middle")
       .attr("dy", "0.3em")
-      .style("fill", "black");
+      .style("fill", "black")
+      .style("font-size", d => {
+        // Calculer la taille de police en fonction de la taille de la bulle
+        const fontSize = Math.min(2 * (d.size || 29), 20);
+        return `${fontSize}px`;
+      })
+      .style("pointer-events", "none") // Empêcher le texte d'interférer avec les événements de la souris sur la bulle
+      .call(wrapText); // Appeler la fonction wrapText pour gérer le texte long
 
+      function wrapText(selection) {
+        selection.each(function(d) {
+          const text = d3.select(this);
+          const radius = (d.size || 29) * 1.3;
+          const maxWidth = radius * 2; // Largeur maximale pour le texte
+          const lineHeight = 1.2; // Hauteur de ligne
+          const words = d.name.split(/\s+/).reverse();
+          let word;
+          let line = [];
+          let lineNumber = 0;
+          const y = text.attr("y");
+          const dy = parseFloat(text.attr("dy"));
+          let tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+      
+          while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > maxWidth && line.length > 1) {
+              line.pop();
+              tspan.text(line.join(" "));
+              line = [word];
+              tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+            }
+          }
+        });
+      }
+      
       const simulation = d3.forceSimulation(data)
       .force("x", d3.forceX().strength(0.05)) // Force horizontale
       .force("y", d3.forceY().strength(0.05)) // Force verticale
